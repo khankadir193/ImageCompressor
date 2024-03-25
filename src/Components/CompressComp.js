ad6460485d997824e88ee1606ac566aee57870af
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-03-25 16:30:04 +0530

    compressor-app

---
+++ src/Components/CompressComp.js
@@ -0,0 +1,28 @@
+import React,{useState} from 'react';
+import { ImageCompressor } from 'image-compressor';
+
+export const CompressComp = () => {
+    const [compressedImage, setCompressedImage] = useState(null);
+
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    const compressor = new ImageCompressor();
+    const compressedFile = await compressor.compress(file);
+    setCompressedImage(compressedFile);
+  };
+    return (
+        <div>
+            <div>
+                <input type="file" accept="image/*" onChange={handleImageUpload} />
+                {compressedImage && (
+                    <div>
+                        <img src={URL.createObjectURL(compressedImage)} alt="Compressed" />
+                        <a href={URL.createObjectURL(compressedImage)} download="compressed_image.jpg">
+                            Download Compressed Image
+                        </a>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+}
